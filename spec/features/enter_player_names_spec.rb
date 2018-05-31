2de51d2db97710b0e58d23c13e0461a4e4f9394a
Laura Petre <laura.d.petre@gmail.com>
2018-05-31 18:23:21 +0100

    DRY with helper method

--- spec/features/enter_player_names_spec.rb
+++ spec/features/enter_player_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'enter player names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in 'player_1_name', with: 'Batman'
-    fill_in 'player_2_name', with: 'Joker'
-    click_button 'Submit'
+    sign_in_and_play
     save_and_open_page
     expect(page).to have_content 'Batman vs. Joker'
   end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature "view opponent's hitpoints" do
   scenario 'view hitpoints'do
-    visit('/')
-    fill_in 'player_1_name', with: 'Sauron'
-    fill_in 'player_2_name', with: 'Gollum'
-    click_button 'Submit'
-    expect(page).to have_content 'Sauron: 300HP'
+    sign_in_and_play
+    expect(page).to have_content 'Batman: 300HP'
   end
 end
